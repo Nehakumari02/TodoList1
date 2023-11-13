0f4fa08dfe50f43fe88483e004ec7793f9175a79
Neha Kumari <76866948+Nehakumari02@users.noreply.github.com>
2023-11-13 22:01:58 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const todoModel=require('./model/todolist')
 const app=express()
 app.use(express.json())
 app.use(cookieParser())
+app.options('*', cors());
 app.use(cors({
     credentials: true,
     origin: ["https://todo-list1-client.vercel.app"],
