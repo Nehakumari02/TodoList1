d809bd7348277125b6127a87b405022276e8ba4a
Neha Kumari <76866948+Nehakumari02@users.noreply.github.com>
2023-11-13 19:16:15 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
     credentials: true,
-    origin: ["http://localhost:5173"],
+    origin: ["https://todolist12.vercel.app/"],
     methods: ["GET", "POST","PUT" ,"DELETE"],
 
    
